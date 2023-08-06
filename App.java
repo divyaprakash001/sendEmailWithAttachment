5683e1bd321da2af782fb8fcca5a73ad0f0e571a
Divya Prakash <95012051+divyaprakash001@users.noreply.github.com>
2023-08-06 13:05:09 +0530

    Update App.java

--- App.java
+++ App.java
@@ -28,72 +28,10 @@ public class App {
 		String to = "divyaprakashara99@gmail.com";
 		String from = "divyaprakashara09@gmail.com";
 
-//		sendEmail(message, subject, to, from);
-
-//		sendAttach(message, subject, to, from);
-
-		sendHtml(message, subject, to, from);
-
+		sendAttach(message, subject, to, from);
+		
 	}
 
-//	**********************************************************************************
-
-	private static void sendHtml(String message, String subject, String to, String from) {
-//	variable for gmail host
-		String host = "smtp.gmail.com";
-
-//	get the system properties
-		Properties properties = System.getProperties();
-		System.out.println("Properties :: " + properties);
-
-//	setting important information to properties object
-
-//	host set
-		properties.put("mail.smtp.host", host);
-		properties.put("mail.smtp.port", "465"); // google smtp port = 465
-		properties.put("mail.smtp.ssl.enable", "true");
-		properties.put("mail.smtp.auth", "true");
-
-		// step 1. to get the session object
-		Session session = Session.getInstance(properties, new Authenticator() {
-
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				// TODO Auto-generated method stub
-				return new PasswordAuthentication("divyaprakashara09@gmail.com", "rfwrxtsndmutqowv");
-			}
-
-		});
-
-		session.setDebug(true);
-
-//	step 2. compose the message [text, multi media]
-
-		MimeMessage m_message = new MimeMessage(session);
-
-		try {
-//	from email id
-			m_message.setFrom(from);
-
-//		adding recipient
-			m_message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-//		adding subject to message
-			m_message.setSubject(subject);
-
-//		adding text to message
-			m_message.setContent("<h1><mark>This</mark> is a html file.</h1>", "text/html");
-
-//	step 3. 	send the message using transport class
-			Transport.send(m_message);
-
-			System.out.println("Message sent successfully");
-
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-
-	}
 
 //	***************************************************************************************
 
@@ -181,64 +119,5 @@ public class App {
 
 	}
 
-//	*************************************************************************************
-	// this method is responsible to send email
-	@SuppressWarnings("unused")
-	private static void sendEmail(String message, String subject, String to, String from) {
 
-//		variable for gmail host
-		String host = "smtp.gmail.com";
-
-//		get the system properties
-		Properties properties = System.getProperties();
-		System.out.println("Properties :: " + properties);
-
-//		setting important information to properties object
-
-//		host set
-		properties.put("mail.smtp.host", host);
-		properties.put("mail.smtp.port", "465"); // google smtp port = 465
-		properties.put("mail.smtp.ssl.enable", "true");
-		properties.put("mail.smtp.auth", "true");
-
-		// step 1. to get the session object
-		Session session = Session.getInstance(properties, new Authenticator() {
-
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				// TODO Auto-generated method stub
-				return new PasswordAuthentication("divyaprakashara09@gmail.com", "rfwrxtsndmutqowv");
-			}
-
-		});
-
-		session.setDebug(true);
-
-//		step 2. compose the message [text, multi media]
-
-		MimeMessage m_message = new MimeMessage(session);
-
-		try {
-//		from email id
-			m_message.setFrom(from);
-
-//			adding recipient
-			m_message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-//			adding subject to message
-			m_message.setSubject(subject);
-
-//			adding text to message
-			m_message.setText(message);
-
-//		step 3. 	send the message using transport class
-			Transport.send(m_message);
-
-			System.out.println("Message sent successfully");
-
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-
-	}
 }
